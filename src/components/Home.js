04b158cc70f8277203de42ec1ba2dc3f00a3669c
almirsulejmanovic <16432927+almirsulejmanovic@users.noreply.github.com>
2021-02-18 18:12:28 -0600

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Button, ButtonGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faOilCan, faRing, faCarBattery, faCar, faList } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
+    const oilIcon = <FontAwesomeIcon icon={faOilCan} />
+    const tireIcon = <FontAwesomeIcon icon={faRing} />
+    const batteryIcon = <FontAwesomeIcon icon={faCarBattery} />
+    const vinIcon = <FontAwesomeIcon icon={faCar} />
+    const listIcon = <FontAwesomeIcon icon={faList} />
+
     return (
         <div className="home-container">
             <ButtonGroup vertical>
-                <Link to="/oilchange"><Button className="home-buttons">New Oil Change</Button></Link>
-                <Link to="/tires"><Button className="home-buttons">New Tires</Button></Link>
-                <Link to="/battery"><Button className="home-buttons">New Battery</Button></Link>
-                <Link to="/VINDecoder"><Button className="home-buttons">VIN Decoder</Button></Link>
-                <Link to="/vehiclelist"><Button className="home-buttons">Vehicle List</Button></Link>
+                <Link to="/oilchange"><Button className="home-buttons"><span className="home-fa-icons">{oilIcon}</span>New Oil Change</Button></Link>
+                <Link to="/tires"><Button className="home-buttons"><span className="home-fa-icons">{tireIcon}</span>New Tires</Button></Link>
+                <Link to="/battery"><Button className="home-buttons"><span className="home-fa-icons">{batteryIcon}</span>New Battery</Button></Link>
+                <Link to="/VINDecoder"><Button className="home-buttons"><span className="home-fa-icons">{vinIcon}</span>VIN Decoder</Button></Link>
+                <Link to="/vehiclelist"><Button className="home-buttons"><span className="home-fa-icons">{listIcon}</span>Vehicle List</Button></Link>
             </ButtonGroup>
         </div>
     )
